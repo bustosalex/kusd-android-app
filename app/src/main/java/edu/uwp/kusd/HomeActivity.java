8a201395bdd07e83a594575f7c184a1967a83635
davisdj <davis159@rangers.uwp.edu>
2016-10-10 22:20:00 -0500

    Updated HomeActivity for New Icons
    Updated HomeActivity to use the new icons.

--- app/src/main/java/edu/uwp/kusd/HomeActivity.java
+++ app/src/main/java/edu/uwp/kusd/HomeActivity.java
@@ -20,14 +20,23 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         List<AppSection> appSections = new ArrayList<>();
+        AppSection news = new AppSection(R.drawable.ic_newspaper_black_48dp, NewsActivity.class);
         appSections.add(news);
+        AppSection calendar = new AppSection(R.drawable.ic_calendar_black_48dp, CalendarActivity.class);
         appSections.add(calendar);
+        AppSection lunch = new AppSection(R.drawable.ic_food_apple_black_48dp, LunchActivity.class);
         appSections.add(lunch);
+        AppSection schools = new AppSection(R.drawable.ic_school_black_48dp, SchoolsActivity.class);
         appSections.add(schools);
+        AppSection socialMedia = new AppSection(R.drawable.ic_facebook_box_black_48dp, SocialMediaActivity.class);
         appSections.add(socialMedia);
+        AppSection features = new AppSection(R.drawable.ic_star_black_48dp, FeaturesActivity.class);
         appSections.add(features);
+        AppSection textAlert = new AppSection(R.drawable.ic_message_reply_text_black_48dp, TextAlertActivity.class);
         appSections.add(textAlert);
+        AppSection boardMembers = new AppSection(R.drawable.ic_bank_black_48dp, BoardMembersActivity.class);
         appSections.add(boardMembers);
+        AppSection infiniteCampus = new AppSection(R.drawable.ic_infinity_black_48dp, InfiniteCampusActivity.class);
         appSections.add(infiniteCampus);
 
         recyclerView = (RecyclerView) findViewById(R.id.home_activty_icon_grid);
