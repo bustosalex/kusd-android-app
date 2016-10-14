92a7dfd36790f7956b673b3c202f1c312bb5fa36
ChristianMayer21 <mayerchris10@yahoo.com>
2016-10-14 18:31:48 -0500

    Lunch activity in progress-test

--- app/src/main/java/edu/uwp/kusd/LunchActivity.java
+++ app/src/main/java/edu/uwp/kusd/LunchActivity.java
@@ -1,13 +1,51 @@
 package edu.uwp.kusd;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+
+
 
 public class LunchActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        LunchActivity_ViewPageAdapter adapter = new  LunchActivity_ViewPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LunchActivity_Tab_One(), "Elementary School");
+        adapter.addFragment(new LunchActivity_Tab_Two(), "Middle School");
+        //adapter.addFragment(new TabThreeFragment(), "Three");
+        viewPager.setAdapter(adapter);
+
+    }
+
+
 }
