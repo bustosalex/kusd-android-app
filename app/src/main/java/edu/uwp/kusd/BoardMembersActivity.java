e52eab493130b1d47f8785be4e5e641bf90c62f7
davisdj <davis159@rangers.uwp.edu>
2016-10-23 17:49:52 -0500

    Basic Board Member Functionality
    Implemented basic functionality for the board members activity.

--- app/src/main/java/edu/uwp/kusd/BoardMembersActivity.java
+++ app/src/main/java/edu/uwp/kusd/BoardMembersActivity.java
@@ -1,5 +1,7 @@
 package edu.uwp.kusd;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class BoardMembersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_members);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.activity_board_members_fragment_container);
+
+        if (fragment == null) {
+            Fragment BoardMembersFragment = new BoardMembersFragment();
+            fragmentManager.beginTransaction().add(R.id.activity_board_members_fragment_container, BoardMembersFragment).commit();
+        }
     }
 }
