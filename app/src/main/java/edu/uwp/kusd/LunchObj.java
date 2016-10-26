6b6691af82078460b71ea649d4708ff89dca1c5b
ChristianMayer21 <mayerchris10@yahoo.com>
2016-10-25 19:35:41 -0500

    Working on creating parser, have logic created for the xml parser, need to keep working on LunchPullParser

---
+++ app/src/main/java/edu/uwp/kusd/LunchObj.java
@@ -0,0 +1,36 @@
+package edu.uwp.kusd;
+
+/**
+ * Created by Cabz on 10/25/2016.
+ */
+
+public class LunchObj {
+    private String title;
+    private String fileUrl;
+    private  String category;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getfileURL(){
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl){
+        this.fileUrl = fileUrl;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+}
---
+++ app/src/main/java/edu/uwp/kusd/LunchParserPull.java
@@ -0,0 +1,56 @@
+package edu.uwp.kusd;
+
+/**
+ * Created by Cabz on 10/25/2016.
+ */
+
+import java.io.IOException;
+import java.util.List;
+//import com.theopentutorials.android.beans.Employee;
+//import com.theopentutorials.android.xml.XMLPullParserHandler;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+
+public class LunchParserPull {
+
+
+
+
+
+
+
+/*        ListView listView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            listView = (ListView) findViewById(R.id.list);
+
+            List<Employee> employees = null;
+            try {
+                XMLPullParserHandler parser = new XMLPullParserHandler();
+                employees = parser.parse(getAssets().open("employees.xml"));
+                ArrayAdapter<Employee> adapter =
+                        new ArrayAdapter<Employee>(this,R.layout.list_item, employees);
+                listView.setAdapter(adapter);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+
+    }*/
+}
