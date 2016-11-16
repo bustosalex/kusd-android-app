d3a0ac85049b20803a33fb9516cb5cc42de2657d
Eitansgit <eitansphone@gmail.com>
2016-11-16 14:08:00 -0600

    Activity now actually works to a degree, but does not open the app if you have it installed. It just opens the relevant Play Store page.

--- app/src/main/java/edu/uwp/kusd/InfiniteCampusActivity.java
+++ app/src/main/java/edu/uwp/kusd/InfiniteCampusActivity.java
@@ -2,12 +2,59 @@ package edu.uwp.kusd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
+
+
+import static android.content.pm.PackageManager.GET_META_DATA;
+import static android.content.pm.PackageManager.NameNotFoundException;
+
+
 
 public class InfiniteCampusActivity extends AppCompatActivity {
+
+
+    private final String campusExt = "com.infinitecampus";
+    private final String campusExtExt = ".mobilePortal&hl=en";
+    private final String campusURL = "market://details?id=" + campusExt + campusExtExt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infinite_campus);
+
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(campusExt+campusExtExt);
+        if (launchIntent != null) {
+            startActivity(launchIntent);//null pointer check in case package name was not found
+
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Infinite Campus is not installed.\nOpening Play Store.", Toast.LENGTH_LONG);
+            toast.show();
+            Intent campIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(campusURL));
+            try {
+                // Attempt to launch Infinite Campus
+                startActivity(campIntent);
+            } catch (ActivityNotFoundException e) {
+                // Failed to launch, display exception to user
+                raiseException(e.getMessage());
+
+
+            }
+
+        }
+    }
+
+    private void raiseException(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message).setPositiveButton("Dismiss", null);
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
