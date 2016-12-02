ab221cf0f8113e78b40f2d4583aa36c22464aa18
ChristianMayer21 <mayerchris10@yahoo.com>
2016-12-02 15:42:40 -0600

    Added lizzes Pager adapter back in

--- app/src/main/java/edu/uwp/kusd/SchoolsActivity.java
+++ app/src/main/java/edu/uwp/kusd/SchoolsActivity.java
@@ -35,7 +35,7 @@ public class SchoolsActivity extends AppCompatActivity {
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter1 adapter = new PagerAdapter1
+        final PagerAdapter adapter = new PagerAdapter
                 (getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
